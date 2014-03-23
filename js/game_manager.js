dc269d0a6f3b3f7fcede403fc89f278c14659ab3
symmetrygroup <kevinsingh229@gmail.com>
2014-03-23 15:40:10 -0700

    Adding UC 1024

--- js/game_manager.js
+++ js/game_manager.js
@@ -139,8 +139,8 @@ GameManager.prototype.move = function (direction) {
           // Update the score
           self.score += merged.value;
 
-          // The mighty 2048 tile
-          if (merged.value === 2048) self.won = true;
+          // The mighty 1024 tile
+          if (merged.value === 1024) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
